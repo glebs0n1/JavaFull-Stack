1b10b3b89f3c73d95eec31e99430ebd20b4c501b
Gleb <songleb81@gmail.com>
2023-10-29 20:28:33 +0200

    settings adjustment

--- src/main/java/com/example/crud/DataSourceConfig.java
+++ src/main/java/com/example/crud/DataSourceConfig.java
@@ -1,24 +1,24 @@
-//package com.example.crud;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.env.Environment;
-//import org.springframework.jdbc.datasource.DriverManagerDataSource;
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class DataSourceConfig {
-//    @Autowired
-//    private Environment env;
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName("org.firebirdsql.jdbc.FBDriver");
-//        dataSource.setUrl("jdbc:firebirdsql://localhost:3050/C:/Users/gleb/Desktop/PERSONS.FDB");
-//        dataSource.setUsername("sysdba");
-//        dataSource.setPassword("Root");
-//        return dataSource;
-//    }
-//}
+package com.example.crud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.firebirdsql.jdbc.FBDriver");
+        dataSource.setUrl("jdbc:firebirdsql://localhost:3050/C:/Users/gleb/Desktop/PERSONS.FDB");
+        dataSource.setUsername("sysdba");
+        dataSource.setPassword("Root");
+        return dataSource;
+    }
+}
